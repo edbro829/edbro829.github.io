f07052e1b62ba79acc7f8fb720244b7110e97c8a
Edbro829 <35573136+edbro829@users.noreply.github.com>
2019-01-23 10:08:53 -0700

    Update area.js

--- area.js
+++ area.js
@@ -1,1 +1,9 @@
-<input> 
+length: <input id="length">
+width: <input id="width">
+Area: <input id="finalArea">
+<script>
+  function area() {
+  var length = document.getElementById('length').value;
+  var width = document.getElementById('width').value;
+  var finalArea = length * width
+}
